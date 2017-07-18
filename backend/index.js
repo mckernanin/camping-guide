d0ef4f3a237177d10056723fa2dfd9f1d4aadd0a
Henry Kaufman <hkaufman19@gmail.com>
2017-07-18 12:24:22 -0600

    :fire: Removed backend users code

--- backend/index.js
+++ backend/index.js
@@ -2,26 +2,6 @@ const { Router } = require('express');
 
 const router = Router();
 
-const exampleData = [
-  {
-    id: 1,
-    name: 'Tara Simmmons',
-    email: 'tara.simmmons55@example.com'
-  },
-  {
-    id: 2,
-    name: 'Willie Romero',
-    email: 'willie.romero57@example.com'
-  },
-  {
-    id: 3,
-    name: 'Holly Barnes',
-    email: 'holly.barnes11@example.com'
-  }
-];
-
-router.get('/users', (req, res) => res.send(exampleData));
-
 router.use('/enums', require('./routes/Enums'));
 
 router.use('/locations', require('./routes/Location'));
