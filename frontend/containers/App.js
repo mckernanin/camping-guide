760d69bd8161bf950606f8fcf59e92a4737961c9
Henry Kaufman <hkaufman19@gmail.com>
2017-07-26 16:30:01 -0600

    :ticket: Added a grid and updated the home styles

--- frontend/containers/App.js
+++ frontend/containers/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 import { PropTypes } from 'prop-types';
 import { connect } from 'react-redux';
+import Col from 'rfg';
+import styled from 'styled-components';
 
 import { fetchLocationsRequest } from '../redux/modules/location';
 
 import ListItem from '../components/ListItem';
 import Container from '../components/Container';
+
+const VPadded = styled.div`
+  padding: 40px 0;
+`;
 
 @connect(({ location }) => ({ location }), { fetchLocationsRequest })
 export default class App extends Component {
@@ -26,11 +32,18 @@ export default class App extends Component {
     const { location: { locations, loaded, loading } } = this.props;
     return (
       <Container>
-        {loading && <p>Stuff is loading.</p>}
-        {loaded && !locations.length && <p>There was an error loading.</p>}
-        {locations &&
-          locations.map(place =>
-            <ListItem key={place._id} name={place.name} location={place.location} />)}
+        <VPadded>
+          <Col size={4}>
+            {loading && <p>Stuff is loading.</p>}
+            {loaded && !locations.length && <p>There was an error loading.</p>}
+            {locations &&
+              locations.map(place =>
+                <ListItem key={place._id} name={place.name} location={place.location} />)}
+          </Col>
+          <Col size={8}>
+            Some stuff goes here...
+          </Col>
+        </VPadded>
       </Container>
     );
   }
